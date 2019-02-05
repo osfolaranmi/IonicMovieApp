426963f03913c4ed4a4eb2bd8f76acee78f28ddc
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-02-05 12:04:41 +0100

    First Commit

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,29 @@
+import { MovieService } from './../../services/movie.service';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { SearchType } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  results: Observable<any>;
+  searchTerm = '';
+  type: SearchType = SearchType.all;
+ 
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+  }
+
+  searchChanged(){
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+    // this.results.subscribe(res => {
+      
+    // })
+
+  }
+}
